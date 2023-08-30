9b729346ca7c9eb2da0ef9a9c80d7401633fa410
Harshad Sonar <61082703+Harshadsonar@users.noreply.github.com>
2023-08-30 13:59:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,124 +1,34 @@
 const products = [
-    { id: 1, name: "Product 1", price: 10 },
-    { id: 2, name: "Product 2", price: 20 },
-    { id: 3, name: "Product 3", price: 30 },
-    { id: 4, name: "Product 4", price: 40 },
-    { id: 5, name: "Product 5", price: 50 },
+  { id: 1, name: "Product 1", price: 10 },
+  { id: 2, name: "Product 2", price: 20 },
+  { id: 3, name: "Product 3", price: 30 },
+  { id: 4, name: "Product 4", price: 40 },
+  { id: 5, name: "Product 5", price: 50 },
 ];
 
+// DOM elements
 const productList = document.getElementById("product-list");
-const cartList = document.getElementById("cart-list");
-const clearCartButton = document.getElementById("clear-cart-btn");
 
-function getCartFromStorage() {
-    const cartJSON = sessionStorage.getItem("cart");
-    return cartJSON ? JSON.parse(cartJSON) : [];
-}
-
-// Function to save the cart data to session storage
-function saveCartToStorage(cart) {
-    sessionStorage.setItem("cart", JSON.stringify(cart));
-}
-
-// Function to add item to the cart
-function addToCart(productId) {
-    const cart = getCartFromStorage();
-    const product = products.find((item) => item.id === productId);
-
-    if (product) {
-        cart.push(product);
-        saveCartToStorage(cart);
-        renderCart();
-    }
-}
-
-// Function to remove item from the cart
-function removeFromCart(productId) {
-    const cart = getCartFromStorage();
-    const updatedCart = cart.filter((item) => item.id !== productId);
-    saveCartToStorage(updatedCart);
-    renderCart();
-}
-
-// Function to clear the cart
-function clearCart() {
-    sessionStorage.removeItem("cart");
-    renderCart();
-}
-
-// Function to render the product list
+// Render product list
 function renderProducts() {
-    products.forEach((product) => {
-        const li = document.createElement("li");
-        li.classList.add("product");
-        li.innerHTML = `${product.name} - $${product.price} <button class="add-to-cart-btn" data-id="${product.id}">Add to Cart</button>`;
-        productList.appendChild(li);
-    });
+  products.forEach((product) => {
+    const li = document.createElement("li");
+    li.innerHTML = `${product.name} - $${product.price} <button class="add-to-cart-btn" data-id="${product.id}">Add to Cart</button>`;
+    productList.appendChild(li);
+  });
 }
 
-// Function to render the cart items
-function renderCart() {
-    const cart = getCartFromStorage();
+// Render cart list
+function renderCart() {}
 
-    // Clear existing cart items
-    cartList.innerHTML = "";
+// Add item to cart
+function addToCart(productId) {}
 
-    // Render cart items
-    cart.forEach((item) => {
-        const li = document.createElement("li");
-        li.classList.add("cart-item");
-        li.innerHTML = `${item.name} - $${item.price} <button class="remove-from-cart-btn" data-id="${item.id}">Remove</button>`;
-        cartList.appendChild(li);
-    });
-}
+// Remove item from cart
+function removeFromCart(productId) {}
 
-// Event listener for adding products to cart
-productList.addEventListener("click", (event) => {
-    if (event.target.classList.contains("add-to-cart-btn")) {
-        const productId = parseInt(event.target.getAttribute("data-id"));
-        addToCart(productId);
-    }
-});
-
-// Event listener for removing products from cart
-cartList.addEventListener("click", (event) => {
-    if (event.target.classList.contains("remove-from-cart-btn")) {
-        const productId = parseInt(event.target.getAttribute("data-id"));
-        removeFromCart(productId);
-    }
-});
-
-// Event listener for clearing the cart
-clearCartButton.addEventListener("click", clearCart);
-
-() => {
-    // Click the "Add to Cart" button for the first product
-    cy.get("ul#product-list").children("li").first().children("button").click();
-
-    // Ensure the correct products are in the cart
-    const expectedProducts = [
-        { id: 1, name: "Product 1", price: 10 },
-        { id: 5, name: "Product 5", price: 50 },
-    ];
-
-    // Check the cart in session storage
-    cy.window().its("sessionStorage").should((sessionStorage) => {
-        const cart = JSON.parse(sessionStorage.getItem("cart")) || [];
-
-        // Custom assertion to check for the presence of expected products in the cart
-        expect(cart).to.have.length(expectedProducts.length);
-
-        expectedProducts.forEach((expectedProduct) => {
-            expect(cart).to.deep.include(expectedProduct);
-        });
-    });
-}
-
-
-
-
-
-
+// Clear cart
+function clearCart() {}
 
 // Initial render
 renderProducts();
